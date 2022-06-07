08afa1b426f6b6ce77e222688087b37a7d15da27
wrudder <william.rudder1@gmail.com>
2022-06-07 12:06:36 -0700

    assignment

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -3,10 +3,7 @@
 class ProductController < ApplicationController
   def index
        
-    # originally i was going to go with an sql query that would get an average for the ratings however i feel asthough this is an expensive task EVERYTIME the product page loads. I've decided instead to go with adding a column to the products table for which average rating is updated upon every review submission.
-
-    # @products = Product.select('* FROM products p inner join (SELECT product_id, AVG(rating) as avg_rating
-    # FROM reviews GROUP BY product_id) as x on p.id = x.product_id ORDER BY x.avg_rating DESC')
+# note: i think a better way to do this would be to add a column to the product table "average_rating" - this could update everytime a review is submitted and would be far less expensive.
 
    @products = Product.joins(
     "INNER JOIN (#{Review.select('product_id, AVG(rating) AS avg_rating').group('product_id').to_sql}) AS reviews ON reviews.product_id = products.id"
